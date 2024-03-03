7641614564eb1b94cd2f62d208890a7077fc9cc6
bogcretu <cretumbogdan@gmail.com>
2024-03-03 21:38:59 +0200

    init

---
+++ setup.sh
@@ -0,0 +1,7 @@
+python3 -m venv .venv_web3
+source .venv_web3/bin/activate
+python3 -m pip install -r requirements.txt
+echo "export PRIVATE_KEY=<>" > .env
+echo "export WEB3_INFURA_PROJECT_ID=<>" >> .env
+echo "Do replace <> with the actual values..."
+
